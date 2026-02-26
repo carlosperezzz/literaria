6e6edc6bed5576985c8f2dbc37e2eee07593d2ca
carlosperezzz <capega112@gmail.com>
2026-02-26 17:27:19 +0100

    Update route.js

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -8,7 +8,7 @@ export async function POST(request) {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      model: 'llama3-8b-8192',
+      model: 'llama-3.1-8b-instant',
       messages: [
         {
           role: 'system',
